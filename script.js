72dfa316d7a7a47259f4ee62af1e367d1f3c011b
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-10-01 23:26:51 -0700

    add initial html, css files and javascript file with pseudo code

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Code Quiz -Pseudo code
+
+—When start button is clicked, timer starts and a question is presented.
+—Once answer it, - correctly or incorrectly - another one is presented
+—if answered incorrectly, time is subtracted from clock.
+—game is over , when either all questions are answered or timer is zero
+—save the initials and score , when the game is over.
+—it should store the high score if you play multiple times. - so that sort of information will need to be persistent.
+
+—When either view high scores button is clicked ,it shud display the high scores page with the score board and also with Go back and clear high scores buttons.
+—the scores with initials should listed in a ordered list and display above the buttons(go back and clear high scores)
+—when clear high scores button is clicked, the score board shud be cleared.
+—when Go back is clicked, it should return to the home page of the code quiz.
+—when a question is answered, the result (correct or wrong) shud be displayed along with the next question presented but below it and line .
+—after the game is over, All done page should be played with final score and enter initial text input box and submit button.
+—once initial are submitted, it shud display the high scores with the list of score and 2 buttons.(view high scores page)
+—questions related to javascript coding.
+—hide the information and display as per the requirement or as clicked
+
+
+
+
+- final score will be the time left.
+
+time===75 seconds
+Question 5
+Wrong answer deduct=10sec */
